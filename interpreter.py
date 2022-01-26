90a91e269c3d5067e9f7da28aa6b26bdca48f4e4
jmcb <joelsgp@protonmail.com>
2022-01-26 14:48:58 +0000

    Finish first draft of interpreter

--- interpreter.py
+++ interpreter.py
@@ -13,7 +13,7 @@ REGEX_OPERATION = re.compile(r'^(AND|XOR) ([A-Z]+|[0-9]+) ([A-Z]+|[0-9]+)$')
 
 Namespace = dict[str, int]
 Lines = list[str]
-Stepthrough = list[tuple[int, Namespace]]
+Stepthrough = list[tuple[int, str, Namespace]]
 
 
 def process_lines(source: str) -> Lines:
@@ -31,8 +31,16 @@ def get_addresses(lines: list[str]) -> Namespace:
     return addresses
 
 
-def extract_argument(arg: str, variables: Namespace) -> int:
-    if arg
+def extract_arg(arg: str, variables: Namespace) -> int:
+    if arg.isdigit():
+        return int(arg)
+    else:
+        return variables[arg]
+
+
+def update_stepthrough(stepthrough: Stepthrough, instruction: int, line: str, variables: Namespace):
+    new_step = (instruction, line, copy(variables))
+    stepthrough.append(new_step)
 
 
 def execute_lines(lines: list[str]) -> Stepthrough:
@@ -43,33 +51,35 @@ def execute_lines(lines: list[str]) -> Stepthrough:
     instruction_max = len(lines)
     addresses = get_addresses(lines)
 
+    # the duplicated loop continuation blocks are a bit nasty
     while True:
         # end of file
         if instruction >= instruction_max:
             break
 
+        l = lines[instruction]
+
         # address declarations are skipped
         if instruction in addresses.values():
+            update_stepthrough(stepthrough, instruction, l, variables)
             instruction += 1
             continue
-
-        l = lines[instruction]
 
         # assignment
         match = REGEX_ASSIGNMENT.match(l)
         if match is not None:
             name = match[1]
             assignee = match[2]
-            if assignee.isdigit():
-                variables[name] = int(assignee)
-            else:
-                variables[name] = variables[assignee]
+            assignee_ex = extract_arg(assignee, variables)
+            variables[name] = assignee_ex
+            update_stepthrough(stepthrough, instruction, l, variables)
             instruction += 1
             continue
 
         # jump
         match = REGEX_JUMP.match(l)
         if match is not None:
+            update_stepthrough(stepthrough, instruction, l, variables)
             if last_calculation == 0:
                 jump_address = match[1]
                 instruction = addresses[jump_address]
@@ -81,11 +91,18 @@ def execute_lines(lines: list[str]) -> Stepthrough:
         match = REGEX_OPERATION.match(l)
         if match is not None:
             arg_left = match[2]
-            if arg_left.isdigit():
             arg_right = match[3]
+            arg_left_ex = extract_arg(arg_left, variables)
+            arg_right_ex = extract_arg(arg_right, variables)
             op = match[1]
-            # if op == 'AND':
-            #     last_calculation =
-
+            if op == 'AND':
+                last_calculation = arg_left_ex & arg_right_ex
+            elif op == 'XOR':
+                last_calculation = arg_right_ex ^ arg_right_ex
+            if arg_left != arg_left_ex:
+                variables[arg_left] = last_calculation
+            update_stepthrough(stepthrough, instruction, l, variables)
+            instruction += 1
+            continue
 
     return stepthrough
