612a6fc1be7d7864cb7e006024ff7af6990257bb
jmcb <joelsgp@protonmail.com>
2022-01-26 23:08:28 +0000

    Try some stuff idk

--- ADDER.asm
+++ ADDER.asm
@@ -5,14 +5,16 @@ Y = 35  // 00100011
 
 // program
 SUM = X
-
+CARRY = Y
 
 Addstart:
     XOR SUM Y
-    AND Y X
+    AND CARRY X
+    XOR CARRY SUM
     JPZ addend
 
     X = SUM
+    Y = CARRY
     AND 0 0
     JPZ addstart
 
