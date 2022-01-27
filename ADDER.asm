f94e84cde83e9cfdec3ed710c94186512336ed99
jmcb <joelsgp@protonmail.com>
2022-01-27 16:10:53 +0000

    Basis of new solution

--- ADDER.asm
+++ ADDER.asm
@@ -6,16 +6,28 @@ Y = 35  // 00100011
 // program
 SUM = X
 
-
 Addstart:
+    // XOR for sum
     XOR SUM Y
+
+    // AND, then left shift for carry
     AND Y X
+    CARRY = 0
+    // BIT 2^0
+    A = Y
+    XOR A 254
+    JPZ zero
+    XOR CARRY 2
+    Zero:
+
+
+    // return if no carry
     JPZ addend
 
+    // loop if carry
     X = SUM
     AND 0 0
     JPZ addstart
-
 
 Addend:
     X = SUM  // 00110001
