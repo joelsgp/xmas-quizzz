49e82d8a65a5a985a0b800568c89affe38439608
jmcb <joelsgp@protonmail.com>
2022-01-27 17:57:30 +0000

    Improved export script

--- export.sh
+++ export.sh
@@ -2,3 +2,9 @@
 git archive -o quizzz.tar.gz HEAD
 git archive -o quizzz.zip HEAD
 git bundle create quizzz.bundle main
+# bypass email attachment limitations
+quizzz_files='quizzz.bundle quizzz.tar.gz quizzz.zip'
+for quizzz in $quizzz_files
+do
+    gpg -a --batch --yes --symmetric --passphrase "BOB" $quizzz
+done
