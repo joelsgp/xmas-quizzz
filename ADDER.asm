5e99abae262c889be14b6d8e55277ae597b24a36
jmcb <joelsgp@protonmail.com>
2022-01-27 16:16:49 +0000

    Bit shift copy pasting work

--- ADDER.asm
+++ ADDER.asm
@@ -13,15 +13,51 @@ Addstart:
     // AND, then left shift for carry
     AND Y X
     CARRY = 0
-    // BIT 2^0
+    // bit 2^0
     A = Y
     XOR A 254
-    JPZ zero
+    JPZ shiftzero
     XOR CARRY 2
-    Zero:
-
+    Shiftzero:
+    // bit 2^1
+    A = Y
+    XOR A 253
+    JPZ shiftone
+    XOR CARRY 4
+    Shiftone:
+    // bit 2^2
+    A = Y
+    XOR A 251
+    JPZ shifttwo
+    XOR CARRY 4
+    Shifttwo:
+    // bit 2^3
+    A = Y
+    XOR A 247
+    JPZ shiftthree
+    XOR CARRY 8
+    Shiftthree:
+    // bit 2^4
+    A = Y
+    XOR A 239
+    JPZ shiftfour
+    XOR CARRY 16
+    Shiftfour:
+    // bit 2^5
+    A = Y
+    XOR A 223
+    JPZ shiftfive
+    XOR CARRY 32
+    Shiftfive:
+    // bit 2^6
+    A = Y
+    XOR A 191
+    JPZ shiftsix
+    XOR CARRY 64
+    Shiftsix:
 
     // return if no carry
+    AND CARRY 1
     JPZ addend
 
     // loop if carry
